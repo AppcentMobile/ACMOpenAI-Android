f9a91a74d9d81ad67c2d002738e9c9603895bf43
Eren <eren.alparslan@appcent.mobi>
2023-04-07 20:29:29 +0300

    -Minor changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,34 +11,33 @@ buildscript {
     }
 }
 plugins {
+    id("java")
     id("maven-publish")
     id("java-library")
-    id("java")
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
 }
 
 group = "mobi.appcent"
 version = "1.0.0"
 
-apply plugin: "kotlin"
-
 repositories {
     mavenCentral()
+    maven("https://plugins.gradle.org/m2/")
+    maven("https://maven.google.com")
 }
 
 dependencies {
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlinVersion}"
-    implementation "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlinVersion}"
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlinVersion}")
 
     //OkHtpp
-    implementation platform("com.squareup.okhttp3:okhttp-bom:4.10.0")
-    implementation "com.squareup.okhttp3:okhttp"
-    implementation "com.squareup.okhttp3:logging-interceptor"
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
 
     //Gson
-    implementation "com.google.code.gson:gson:2.10.1"
+    implementation("com.google.code.gson:gson:2.10.1")
 }
-
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "11"
@@ -50,13 +49,12 @@ tasks {
 
 publishing {
     publications {
-
-        maven(MavenPublication)  {
+        create<MavenPublication>("maven") {
             groupId = "mobi.appcent"
             artifactId = "ACMOpenAI-Android"
             version = "1.0.0"
 
-            from components.java
+            from(components["java"])
         }
     }
 }
